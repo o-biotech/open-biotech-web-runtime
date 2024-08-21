056413121381a503a47b6c37204ded1def4d25b2
ttrichar <ttrichardson92@gmail.com>
2024-08-21 11:46:07 -0600

    logging

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 import { start } from '@fathym/eac/runtime';
 import { config, configure } from './configs/eac-runtime.config.ts';
 
+Deno.stat(Deno.env.get('O_BIOTECH_DENO_KV_PATH') ?? '/home/denokv/o-biotech.db')
+
+console.log(Deno.env.get('O_BIOTECH_DENO_KV_PATH'))
+
 await start(await config, configure);
