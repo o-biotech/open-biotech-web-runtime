3cd0b80db473cd39057364c32e047c086a56464b
ttrichar <ttrichardson92@gmail.com>
2024-08-22 09:31:55 -0600

    debugging

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 import { start } from '@fathym/eac/runtime';
 import { config, configure } from './configs/eac-runtime.config.ts';
 
-Deno.stat(Deno.env.get('O_BIOTECH_DENO_KV_PATH') ?? '/home/denokv/o-biotech.db')
+const info = await Deno.stat(Deno.env.get('O_BIOTECH_DENO_KV_PATH') ?? '/home/denokv/o-biotech.db')
 
+console.log(`IsFile: ${info.isFile}`)
+console.log(`isDirectory: ${info.isDirectory}`)
+console.log(`Size: ${info.size}`)
 console.log(Deno.env.get('O_BIOTECH_DENO_KV_PATH'))
 
 await start(await config, configure);
